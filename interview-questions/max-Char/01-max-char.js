94e4cd5e3653f59aeb9b41fad035a15c484e802d
AryanKuAg <alemantrixaryan@gmail.com>
2022-06-05 13:59:20 +0530

    max-char added

---
+++ interview-questions/max-Char/01-max-char.js
@@ -0,0 +1,23 @@
+const maxChar = (str) => {
+    let obj = {}
+    for(const i of str){
+        if(obj.hasOwnProperty(i)){
+            obj[i] += 1;
+        }else{
+            obj[i] = 1; 
+        }
+    }
+    // map done
+    let maxChar, maxCount = 0  ;
+    for (let k in obj){
+        let tem = maxCount
+        maxCount = Math.max(maxCount, obj[k])
+        if ( tem !== maxCount){
+            maxChar = k
+        }
+    }
+
+    return maxChar;
+}
+
+console.log(maxChar('aaaaabbbbbbbaaabbcsdwrdd'))
