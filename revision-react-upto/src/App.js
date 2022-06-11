0fe676903e6e7069b67b96f93cc12d370d140f09
AryanKuAg <alemantrixaryan@gmail.com>
2022-06-11 17:00:10 +0530

    revision

---
+++ revision-react-upto/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./App.module.css"
+import Sample from "./Sample"
+
+function App() {
+  return (
+    <div >
+      <Sample name = 'aryan button my friend'/>
+      <p className={styles.panel}>This is it my friend</p> 
+    </div>
+  )
+}
+
+export default App
+
+// Dear Animal Fellowship stuffs,
+// My name is Aryan, and I am really interested in the MERN Stack/Web development and would like to have more information about that. I've tried and applied to many companies in my country India but they want a college degree. I've also built projects for companies but they never responded back like accepted or rejected. I gut says they don't support me. I rely on you. Maybe you can help. I'm hard working. I can work as many as you want. Please let me know what are your thoughts about me. 
+// Thank you.
---
+++ revision-react-upto/src/Sample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sample(props) {
+  return (
+    <button>{props.name}</button>
+  )
+}
+
+export default Sample
