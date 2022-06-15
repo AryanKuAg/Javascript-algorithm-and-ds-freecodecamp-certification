2019bcb3b0d7d03b600f5c4669a748dbb9d46666
AryanKuAg <alemantrixaryan@gmail.com>
2022-06-15 14:25:54 +0530

    submit button validity added

--- form-and-user-input-react/src/components/BasicForm.js
+++ form-and-user-input-react/src/components/BasicForm.js
@@ -1,12 +1,17 @@
 import useInput from "../use-input";
 
 const BasicForm = (props) => {
-  const {value: firstName, isValid : enteredFirstNameIsValid, isError: isFirstNameError, inputChangeHandler : firstNameInputHandler, inputBlurHandler: firstNameBlurHandler, reset: resetFirstName } = useInput((d) => d.length > 0)
+  const {value: firstName, isValid : enteredFirstNameIsValid, isError: isFirstNameError, inputChangeHandler : firstNameInputHandler, inputBlurHandler: firstNameBlurHandler, reset: resetFirstName } = useInput((d) => d.trim().length > 0)
 
-  const {value: lastName, isValid: enteredLastNameIsValid, isError: isLastNameError, inputChangeHandler: lastNameInputHandler, inputBlurHandler: lastNameBlurHandler, reset: resetLastName} = useInput((d) => d.length > 0)
+  const {value: lastName, isValid: enteredLastNameIsValid, isError: isLastNameError, inputChangeHandler: lastNameInputHandler, inputBlurHandler: lastNameBlurHandler, reset: resetLastName} = useInput((d) => d.trim().length > 0)
 
   const {value: email, isValid: enteredEmailIsValid, isError: isEmailError, inputChangeHandler: emailInputHandler, inputBlurHandler: emailBlurHandler, reset: resetEmail} = useInput((e) => e.includes('@'))
 
+  let isFormValid = false
+
+  if(enteredFirstNameIsValid && enteredLastNameIsValid && enteredEmailIsValid){
+    isFormValid = true;
+  }
 
   const submitFormHandler = () =>  {
     if(isFirstNameError || isLastNameError || isEmailError){
@@ -39,7 +44,7 @@ const BasicForm = (props) => {
         <input type='text' id='name' value={email} onChange={emailInputHandler} onBlur={emailBlurHandler}/>
       </div>
       <div className='form-actions'>
-        <button disabled={isLastNameError || isFirstNameError || isEmailError}type='submit'>Submit</button>
+        <button disabled={!isFormValid}type='submit'>Submit</button>
       </div>
     </form>
   );
