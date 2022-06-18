7bc43eb5104d899b6eab4af21f01c4ff63f352b9
AryanKuAg <alemantrixaryan@gmail.com>
2022-06-19 01:40:52 +0530

    INsert first done

--- interview-questions/LinkedList/linkedList.js
+++ interview-questions/LinkedList/linkedList.js
@@ -5,7 +5,21 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    insertFirst(data){
+        let prev = this.head;
+
+        this.head = new Node(data)
+        this.head.next = prev;
+       
+    }
 }
 
+const nodeOne = new Node(5)
 const list = new LinkedList()
-console.log(list.head)
+list.head = nodeOne
+console.log(list)
+list.insertFirst(50)
+list.insertFirst(5000)
+
+console.log(list)
