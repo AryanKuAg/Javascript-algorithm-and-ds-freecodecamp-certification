80b4c3c04a4d87ffd7d7ce816bc52be29e965c15
AryanKuAg <alemantrixaryan@gmail.com>
2022-06-25 02:17:59 +0530

    routes added

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,7 +1,18 @@
+import {Route, Routes} from 'react-router-dom'
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Routes>
+      <Route path='/aryan' element={<Welcome />} />
+        
+     
+      <Route path='/agrawal' element={<Products />} />
+        
+     </Routes>
+     
     </div>
   );
 }
