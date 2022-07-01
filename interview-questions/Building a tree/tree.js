c8ad6197197d3dc583cb3ab34e617aa1a6caa8d2
AryanKuAg <alemantrixaryan@gmail.com>
2022-07-01 09:34:19 +0530

    breadth first traversal added

--- interview-questions/Building a tree/tree.js
+++ interview-questions/Building a tree/tree.js
@@ -20,7 +20,16 @@ class Tree {
     this.root = null; 
   }
 
-  traverseBFS() {}
+  traverseBFS(fn) {
+    const arr = [this.root]
+
+    while(arr.length){
+      const node =  arr.shift();
+
+      arr.push(...node.children)
+      fn(node)
+    }
+  }
 
   traverseDFS() {}
 }
