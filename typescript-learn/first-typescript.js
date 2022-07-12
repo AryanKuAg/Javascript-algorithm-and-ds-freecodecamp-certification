b50be70931630a151584ba64966c3909e687e2ec
AryanKuAg <alemantrixaryan@gmail.com>
2022-07-12 23:26:05 +0530

    type alias done

--- typescript-learn/first-typescript.js
+++ typescript-learn/first-typescript.js
@@ -2,3 +2,19 @@ var namePrint = function (name) {
     console.log(name);
 };
 namePrint('alemantrix');
+var num;
+var str;
+var bool;
+num = 3432.32;
+str = 'sfaf';
+bool = false;
+// array of boolean
+var myBools = [false, true, false, false, false];
+var myStrings = ['sfd', '', 'sdfksdfsfj', 'kfsdf', 'p'];
+var mynumbers = [342, 5, 2, 35, 235, 2235];
+// objects
+var firts = { college: 'bekar collelge', year: 42332 };
+// type inference
+var one = 3;
+one = 'hey thre';
+one = false;
--- typescript-learn/first-typescript.ts
+++ typescript-learn/first-typescript.ts
@@ -3,3 +3,40 @@ const namePrint = (name: string) => {
 }
 
 namePrint('alemantrix')
+
+let num: number;
+let str : string;
+let bool : boolean;
+
+
+num = 3432.32;
+str = 'sfaf'
+bool = false;
+
+// array of boolean
+
+let myBools: boolean[] = [false, true, false, false, false]
+let myStrings: string[] = ['sfd', '', 'sdfksdfsfj', 'kfsdf','p']
+let mynumbers: number[] = [342,5,2,35,235,2235]
+
+// objects
+
+let firts: {
+    college: string,
+    year: number
+} = {college: 'bekar collelge', year: 42332}
+
+// type inference
+
+let one:number | string| boolean= 3;
+one = 'hey thre'
+one = false;
+
+// type alias
+
+type some = {
+    name: string,
+    age: number
+}
+
+let ss: some;
