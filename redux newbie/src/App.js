6287f223ccb84c56eb92aeb14346fb107d156447
Eduphore <alemantrixaryan@gmail.com>
2022-08-07 01:17:27 +0530

    working with chartjs

--- redux newbie/src/App.js
+++ redux newbie/src/App.js
@@ -1,19 +1,24 @@
-import Counter from './components/Counter';
-import Header from './components/Header'
-import Auth from './components/Auth'
-import {useSelector} from "react-redux"
-import UserProfile from "./components/UserProfile"
+// import Counter from './components/Counter';
+// import Header from './components/Header'
+// import Auth from './components/Auth'
+// import {useSelector} from "react-redux"
+
+// import BubbleChart from "./Charts/BubbleChart";
+import RadarChart from "./Charts/RadarChart";
+
+// import UserProfile from "./components/UserProfile"
 
 function App() {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+  // const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
   return (
-    <div>
-      <Header/>
+    <div style={{ width: "500px", height: "500px" }}>
+      {/* <Header/>
      {!isLoggedIn && <Auth/>}
      {isLoggedIn && <UserProfile />}
-      <Counter />
+      <Counter /> */}
+      {/* <BubbleChart /> */}
+      <RadarChart />
     </div>
-    
   );
 }
 
--- redux newbie/src/components/Counter.js
+++ redux newbie/src/components/Counter.js
@@ -1,41 +1,43 @@
-import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { counterActions } from '../store/redux-store';
-
-
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/redux-store";
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter.counter)
-  console.log('counter', counter)
-  const showToggleButtons = useSelector(state => state.counter.toggle)
-  const dispatch = useDispatch()
+  const counter = useSelector((state) => state.counter.counter);
+  console.log("counter", counter);
+  const showToggleButtons = useSelector((state) => state.counter.toggle);
+  const dispatch = useDispatch();
 
-  console.log(counter, showToggleButtons)
+  console.log(counter, showToggleButtons);
 
-  const toggleCounterHandler =  () => {
-    dispatch(counterActions.toggleButton())
-  }
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleButton());
+  };
 
   const incrementCounter = () => {
-    dispatch(counterActions.increment())
-  }
+    console.log("counter actions", counterActions);
+    dispatch(counterActions.increment());
+  };
 
   const decrementCounter = () => {
-    dispatch(counterActions.decrement())
-  }
+    dispatch(counterActions.decrement());
+  };
 
   const incrementByNumber = () => {
-    dispatch(counterActions.increaseby5({num: 33}))
-  }
-
+    dispatch(counterActions.increaseby5({ num: 33 }));
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
-      {showToggleButtons && <div><button onClick={incrementCounter}>Increment</button>
-      <button onClick={decrementCounter}>Decrement</button>
-      <button onClick={incrementByNumber}>incremnetByNumber</button></div>}
+      {showToggleButtons && (
+        <div>
+          <button onClick={incrementCounter}>Increment</button>
+          <button onClick={decrementCounter}>Decrement</button>
+          <button onClick={incrementByNumber}>incremnetByNumber</button>
+        </div>
+      )}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
